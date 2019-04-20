f331deabadf6e1c36eaa90f69cce9ffdd4d3e58b
Simon Cropp <simon.cropp@gmail.com>
2019-04-20 12:46:55 +1000

    Update GitHubMarkdownProcessorTests.cs

--- src/Tests/GitHubMarkdownProcessor/GitHubMarkdownProcessorTests.cs
+++ src/Tests/GitHubMarkdownProcessor/GitHubMarkdownProcessorTests.cs
@@ -1,11 +1,9 @@
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using ApprovalTests;
 using MarkdownSnippets;
-using ObjectApproval;
 using Xunit;
 using Xunit.Abstractions;
 

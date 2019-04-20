a01fa52d15d6c281b6c8c354d4d121496f30957f
Simon Cropp <simon.cropp@gmail.com>
2019-04-20 16:02:45 +1000

    fix url

--- src/MarkdownSnippets/Processing/HeaderWriter.cs
+++ src/MarkdownSnippets/Processing/HeaderWriter.cs
@@ -10,7 +10,7 @@ static class HeaderWriter
         writer.WriteLine(@"<!--");
         writer.WriteLine(@"This file was generate by MarkdownSnippets.");
         writer.WriteLine($@"Source File: {relativeSource}");
-        writer.WriteLine(@"To change this file edit the source file and then re-run the generation using either the dotnet global tool (https://github.com/SimonCropp/MarkdownSnippets#githubmarkdownsnippets) or using the api (https://github.com/SimonCropp/MarkdownSnippets#running-as-a-unit-test).");
+        writer.WriteLine(@"To change this file edit the source file and then re-run the generation using either the dotnet global tool (https://github.com/SimonCropp/MarkdownSnippets#markdownsnippetstool) or using the api (https://github.com/SimonCropp/MarkdownSnippets#running-as-a-unit-test).");
         writer.WriteLine(@"-->");
     }
 }

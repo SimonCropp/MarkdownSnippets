54321148098f6964341101cca18581050ac9f072
Simon Cropp <simon.cropp@gmail.com>
2019-04-23 08:05:36 +1000

    add GitHubMarkdownProcessor obsolete

---
+++ src/MarkdownSnippets/Obsoletes.cs
@@ -0,0 +1,10 @@
+﻿namespace MarkdownSnippets
+{
+    [ObsoleteEx(
+        TreatAsErrorFromVersion = "10.0",
+        RemoveInVersion = "11.0",
+        ReplacementTypeOrMember = nameof(DirectoryMarkdownProcessor))]
+    public class GitHubMarkdownProcessor
+    {
+    }
+}

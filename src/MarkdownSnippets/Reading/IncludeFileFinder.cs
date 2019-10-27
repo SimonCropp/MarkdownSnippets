6dc05a8c13c952726707be9344e691fa391715a7
Simon Cropp <simon.cropp@gmail.com>
2019-10-27 12:28:12 +1100

    add include file finder

---
+++ src/MarkdownSnippets/Reading/IncludeFileFinder.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MarkdownSnippets
+{
+    public class IncludeFileFinder
+    {
+        DirectoryFilter? directoryFilter;
+
+        public IncludeFileFinder(DirectoryFilter? directoryFilter = null)
+        {
+            this.directoryFilter = directoryFilter;
+        }
+
+        bool IncludeDirectory(string directoryPath)
+        {
+            Guard.DirectoryExists(directoryPath, nameof(directoryPath));
+            var suffix = Path.GetFileName(directoryPath);
+            if (suffix.StartsWith("."))
+            {
+                return false;
+            }
+
+            if (DirectoryExclusions.ShouldExcludeDirectory(suffix))
+            {
+                return false;
+            }
+
+            if (directoryFilter == null)
+            {
+                return true;
+            }
+
+            return directoryFilter(directoryPath);
+        }
+
+        static bool IncludeFile(string path)
+        {
+            var fileName = Path.GetFileName(path);
+            if (fileName.StartsWith("."))
+            {
+                return false;
+            }
+
+            return fileName.EndsWith(".include.md");
+        }
+
+        public List<string> FindFiles(params string[] directoryPaths)
+        {
+            Guard.AgainstNull(directoryPaths, nameof(directoryPaths));
+            var files = new List<string>();
+            foreach (var directoryPath in directoryPaths)
+            {
+                Guard.DirectoryExists(directoryPath, nameof(directoryPath));
+                FindFiles(Path.GetFullPath(directoryPath), files);
+            }
+
+            return files;
+        }
+
+        void FindFiles(string directoryPath, List<string> files)
+        {
+            foreach (var file in Directory.EnumerateFiles(directoryPath)
+                .Where(IncludeFile))
+            {
+                files.Add(file);
+            }
+
+            foreach (var subDirectory in Directory.EnumerateDirectories(directoryPath)
+                .Where(IncludeDirectory))
+            {
+                FindFiles(subDirectory, files);
+            }
+        }
+    }
+}
---
+++ src/Tests/IncludeFileFinder/IncludeFileFinderTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Concurrent;
+using System.IO;
+using System.Linq;
+using MarkdownSnippets;
+using Xunit;
+using Xunit.Abstractions;
+
+public class IncludeFileFinderTests :
+    XunitApprovalBase
+{
+    [Fact]
+    public void Nested()
+    {
+        var directory = Path.Combine(AssemblyLocation.CurrentDirectory, "IncludeFileFinder/Nested");
+        var finder = new IncludeFileFinder();
+        var files = finder.FindFiles(directory);
+        ObjectApprover.Verify(files, Scrubber.Scrub);
+    }
+
+    [Fact]
+    public void Simple()
+    {
+        var directory = Path.Combine(AssemblyLocation.CurrentDirectory, "IncludeFileFinder/Simple");
+        var finder = new IncludeFileFinder();
+        var files = finder.FindFiles(directory);
+        ObjectApprover.Verify(files, Scrubber.Scrub);
+    }
+
+    [Fact]
+    public void VerifyLambdasAreCalled()
+    {
+        var directories = new ConcurrentBag<string>();
+        var targetDirectory = Path.Combine(AssemblyLocation.CurrentDirectory,
+            "IncludeFileFinder/VerifyLambdasAreCalled");
+        var finder = new IncludeFileFinder(
+            directoryFilter: path =>
+            {
+                directories.Add(path);
+                return true;
+            }
+        );
+        finder.FindFiles(targetDirectory);
+        ObjectApprover.Verify(directories.OrderBy(file => file), Scrubber.Scrub);
+    }
+
+    public IncludeFileFinderTests(ITestOutputHelper output) :
+        base(output)
+    {
+    }
+}

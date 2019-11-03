f1e2c10c3a55ba63fd1191789f7c4b2834c1b327
Simon Cropp <simon.cropp@gmail.com>
2019-11-04 10:39:52 +1100

    Update DirectoryMarkdownProcessorTests.cs

--- src/Tests/DirectoryMarkdownProcessor/DirectoryMarkdownProcessorTests.cs
+++ src/Tests/DirectoryMarkdownProcessor/DirectoryMarkdownProcessorTests.cs
@@ -22,7 +22,8 @@ public class DirectoryMarkdownProcessorTests :
                 "Icon",
                 "Credits",
                 "Release Notes"
-            });
+            },
+            directoryFilter: path => !path.Contains("IncludeFileFinder"));
         processor.Run();
     }
 

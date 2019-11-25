34d402992b8b33375b7366795645d7d93d98a3b7
SimonCropp <simon.cropp@gmail.com>
2019-11-25 22:52:58 +1100

    cleanup

---
+++ src/Tests/GlobalSetup.cs
@@ -0,0 +1,20 @@
+﻿using VerifyXunit;
+using Xunit;
+
+[GlobalSetUp]
+public static class GlobalSetup
+{
+    public static void Setup()
+    {
+        Global.ModifySerialization(settings =>
+        {
+            settings.AddExtraSettings(serializerSettings =>
+            {
+                var converters = serializerSettings.Converters;
+                converters.Add(new ProcessResultConverter());
+                converters.Add(new SnippetConverter());
+            });
+        });
+        Global.AddScrubber(Scrubber.Scrub);
+    }
+}

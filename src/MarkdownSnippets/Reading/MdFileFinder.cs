fe2b862bf55745264689fe73c82d9783404bf81c
Simon Cropp <simon.cropp@gmail.com>
2020-08-05 09:47:11 +1000

    handle empty

--- src/MarkdownSnippets/Reading/MdFileFinder.cs
+++ src/MarkdownSnippets/Reading/MdFileFinder.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -25,7 +24,7 @@ namespace MarkdownSnippets
                 return list;
             }
 
-            throw new ArgumentException("Empty documentExtensions.");
+            return defaultExtensions;
         }
 
         public MdFileFinder(DirectoryFilter? directoryFilter = null, IEnumerable<string>? documentExtensions = null)

39f6c201e25ae70f530e1d749739bd92f124e59d
Drew Burlingame <drewburlingame@users.noreply.github.com>
2021-11-27 00:49:38 +0000

    report the file that's missing the snippets (#436)

--- src/MarkdownSnippets/MissingSnippetsException.cs
+++ src/MarkdownSnippets/MissingSnippetsException.cs
@@ -6,9 +6,14 @@ public class MissingSnippetsException :
     public IReadOnlyList<MissingSnippet> Missing { get; }
 
     public MissingSnippetsException(IReadOnlyList<MissingSnippet> missing) :
-        base($"Missing snippets: {string.Join(", ", missing.Select(x => x.Key))}")
+        base($"Missing snippets:{Environment.NewLine}  {Report(missing)}")
     {
         Missing = missing;
+    }
+
+    static string Report(IReadOnlyList<MissingSnippet> missing)
+    {
+        return string.Join($"{Environment.NewLine}  ", missing.GroupBy(m => m.File ?? "file-unknown").Select(g => $"{g.Key}: {string.Join(",", g.Select(s => s.Key))}"));
     }
 
     public override string ToString()

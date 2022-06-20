e4c04ffe6ded5d21c33be89545d433e5b6dffe57
Simon Cropp <simon.cropp@gmail.com>
2022-06-20 21:32:49 +1000

    Dont exclude directories starting with underscore (#493)

--- src/MarkdownSnippets/Reading/Exclusions/DefaultDirectoryExclusions.cs
+++ src/MarkdownSnippets/Reading/Exclusions/DefaultDirectoryExclusions.cs
@@ -5,8 +5,7 @@ public static class DefaultDirectoryExclusions
     public static bool ShouldExcludeDirectory(string path)
     {
         var suffix = Path.GetFileName(path).ToLowerInvariant();
-        return suffix.StartsWith("_") ||
-               suffix.StartsWith(".") ||
+        return suffix.StartsWith(".") ||
                suffix is
                    "packages" or
                    "node_modules" or

db660e519d22aba7fc746aaae4a37ffa4e7087c4
Simon Cropp <simon.cropp@gmail.com>
2023-03-04 20:43:40 +1100

    .

---
+++ src/MarkdownSnippets/SpanSplitter.cs
@@ -0,0 +1,42 @@
+﻿static class SpanSplitter
+{
+    public static Spans SplitBySpace(this CharSpan value)
+    {
+        var indexes = new List<Spans.Index>();
+        var index = 0;
+        int? from = null;
+        while (true)
+        {
+            if (index == value.Length)
+            {
+                if (from != null)
+                {
+                    indexes.Add(new (from.Value, index-from.Value));
+                }
+                break;
+            }
+            var ch = value[index];
+
+            if (ch == ' ')
+            {
+                if (from != null)
+                {
+                    indexes.Add(new (from.Value, index-from.Value));
+                }
+
+                from = null;
+            }
+            else
+            {
+                if (from == null)
+                {
+                    from = index;
+                }
+            }
+
+            index++;
+        }
+
+        return new(value,indexes);
+    }
+}
---
+++ src/MarkdownSnippets/Spans.cs
@@ -0,0 +1,24 @@
+﻿ref struct Spans
+{
+    CharSpan span;
+    readonly List<Index> items;
+
+    public Spans(CharSpan span, List<Index> items)
+    {
+        this.span = span;
+        this.items = items;
+    }
+
+    public record Index(int From, int To);
+
+    public int Length => items.Count;
+
+    public CharSpan this[int index]
+    {
+        get
+        {
+            var tuple = items[index];
+            return span.Slice(tuple.From, tuple.To);
+        }
+    }
+}

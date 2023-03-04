74c0c1a52b9b0e3d236b2658021f6a8ceaf4eae9
Simon Cropp <simon.cropp@gmail.com>
2023-03-04 20:46:38 +1100

    .

---
+++ src/Tests/SpanSplitterTests.cs
@@ -0,0 +1,23 @@
+﻿[UsesVerify]
+public class SpanSplitterTests
+{
+    [Theory]
+    [InlineData("a")]
+    [InlineData("simple")]
+    [InlineData("with Space")]
+    [InlineData("with  Multiple  Spaces")]
+    [InlineData("  withOuterSpaces  ")]
+    [InlineData("  a  ")]
+    [InlineData("  ")]
+    public Task Simple(string value)
+    {
+        var spans = value.AsSpan().SplitBySpace();
+        var list = new List<string>();
+        for (int index = 0; index < spans.Length; index++)
+        {
+            list.Add(spans[index].ToString());
+        }
+        return Verify(list)
+            .UseParameters(value);
+    }
+}

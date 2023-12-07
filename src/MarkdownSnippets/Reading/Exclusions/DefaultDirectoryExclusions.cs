7fa27df6012ed9f596d41a20fd1fd1765c2c82e9
Simon Cropp <simon.cropp@gmail.com>
2023-12-07 11:38:27 +1100

    dont ignore dirs starting eith period (#678)

--- src/MarkdownSnippets/Reading/Exclusions/DefaultDirectoryExclusions.cs
+++ src/MarkdownSnippets/Reading/Exclusions/DefaultDirectoryExclusions.cs
@@ -5,11 +5,11 @@ public static class DefaultDirectoryExclusions
     public static bool ShouldExcludeDirectory(string path)
     {
         var suffix = Path.GetFileName(path).ToLowerInvariant();
-        return suffix.StartsWith('.') ||
-               suffix is
-                   "packages" or
-                   "node_modules" or
-                   "bin" or
-                   "obj";
+        return suffix is
+            ".git" or
+            "packages" or
+            "node_modules" or
+            "bin" or
+            "obj";
     }
 }

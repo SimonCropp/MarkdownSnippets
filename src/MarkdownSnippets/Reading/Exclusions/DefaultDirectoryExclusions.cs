8e4f0a1a1a198645e1840a4b873e23211ba6bdac
Simon Cropp <simon.cropp@gmail.com>
2024-03-26 11:05:12 +1100

    ignore hidden directories (#707)

--- src/MarkdownSnippets/Reading/Exclusions/DefaultDirectoryExclusions.cs
+++ src/MarkdownSnippets/Reading/Exclusions/DefaultDirectoryExclusions.cs
@@ -4,14 +4,22 @@ public static class DefaultDirectoryExclusions
 {
     public static bool ShouldExcludeDirectory(string path)
     {
-        var suffix = Path.GetFileName(path).ToLowerInvariant();
-        return suffix is
+        var suffix = Path
+            .GetFileName(path)
+            .ToLowerInvariant();
+        if (suffix is
             ".git" or
             ".vs" or
             ".idea" or
             "packages" or
             "node_modules" or
             "bin" or
-            "obj";
+            "obj")
+        {
+            return true;
+        }
+
+        var directory = new DirectoryInfo(path);
+        return directory.Attributes.HasFlag(FileAttributes.Hidden);
     }
 }

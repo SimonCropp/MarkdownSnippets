7aee95ecfdffbe1d3bf3da3ca4fe53eae00ee857
khalidabuhakmeh <khalidabuhakmeh@gmail.com>
2025-05-02 14:31:40 -0400

    Refactor Regex initialization with conditional compilation.
    Reorganized Regex initialization using `#if NET8_0_OR_GREATER` to leverage `GeneratedRegex` for .NET 8+ and fallback to static initialization otherwise instance is null.

--- src/MarkdownSnippets/Reading/ExpressiveCode.cs
+++ src/MarkdownSnippets/Reading/ExpressiveCode.cs
@@ -1,5 +1,11 @@
 internal partial class ExpressiveCode
 {
+#if NET8_0_OR_GREATER
+    [GeneratedRegex(@"([a-zA-Z0-9\-_]+)(?:\((.*?)\))?")]
+    protected partial Regex KeyPatternRegex();
+#else
+    protected static readonly Regex KeyPatternRegex = new(@"([a-zA-Z0-9\-_]+)(?:\((.*?)\))?");
+#endif
     public ExpressiveCode() =>
 #if NET8_0_OR_GREATER
         Pattern = KeyPatternRegex();
@@ -8,10 +14,4 @@ internal partial class ExpressiveCode
 #endif
     public static ExpressiveCode Instance { get; } = new();
     public Regex Pattern { get; }
-#if NET8_0_OR_GREATER
-    [GeneratedRegex(@"([a-zA-Z0-9\-_]+)(?:\((.*?)\))?")]
-    protected partial Regex KeyPatternRegex();
-#else
-    protected static readonly Regex KeyPatternRegex = new(@"([a-zA-Z0-9\-_]+)(?:\((.*?)\))?");
-#endif
 }

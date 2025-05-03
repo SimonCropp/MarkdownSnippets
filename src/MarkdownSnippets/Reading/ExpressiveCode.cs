7d7666709c031f2142f89a2d564901cd0a2600da
Simon Cropp <simon.cropp@gmail.com>
2025-05-03 23:01:14 +1000

    Update ExpressiveCode.cs

--- src/MarkdownSnippets/Reading/ExpressiveCode.cs
+++ src/MarkdownSnippets/Reading/ExpressiveCode.cs
@@ -2,12 +2,12 @@ static partial class ExpressiveCode
 {
 #if NET8_0_OR_GREATER
     static ExpressiveCode() =>
-        Pattern = KeyPatternRegex();
+        Pattern = BuildRegex();
 
     public static Regex Pattern { get; }
 
     [GeneratedRegex(@"([a-zA-Z0-9\-_]+)(?:\((.*?)\))?")]
-    private static partial Regex KeyPatternRegex();
+    private static partial Regex BuildRegex();
 #else
     public static readonly Regex Pattern = new(@"([a-zA-Z0-9\-_]+)(?:\((.*?)\))?");
 #endif

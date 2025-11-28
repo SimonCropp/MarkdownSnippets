48d3c31cd9921deaf0828e4379a5663f13b3825b
Simon Cropp <simon.cropp@gmail.com>
2025-11-29 10:02:31 +1100

    refs or cleanup

--- src/Tests/SnippetFileFinderTests.cs
+++ src/Tests/SnippetFileFinderTests.cs
@@ -15,7 +15,7 @@ public class SnippetFileFinderTests
     [Fact]
     public Task Nested()
     {
-        var directory = Path.Combine(AttributeReader.GetProjectDirectory(), "SnippetFileFinder/Nested");
+        var directory = Path.Combine(ProjectFiles.ProjectDirectory, "SnippetFileFinder/Nested");
         var finder = new FileFinder(directory, DocumentConvention.SourceTransform, _ => true, _ => true, _ => true);
         var files = finder.FindFiles();
         return Verify(files.snippetFiles);
@@ -24,7 +24,7 @@ public class SnippetFileFinderTests
     [Fact]
     public Task Simple()
     {
-        var directory = Path.Combine(AttributeReader.GetProjectDirectory(), "SnippetFileFinder/Simple");
+        var directory = Path.Combine(ProjectFiles.ProjectDirectory, "SnippetFileFinder/Simple");
         var finder = new FileFinder(directory, DocumentConvention.SourceTransform, _ => true, _ => true, _ => true);
         var files = finder.FindFiles();
         return Verify(files.snippetFiles);
@@ -36,7 +36,7 @@ public class SnippetFileFinderTests
         var directories = new ConcurrentBag<string>();
         var snippetDirectories = new ConcurrentBag<string>();
         var markdownDirectories = new ConcurrentBag<string>();
-        var directory = Path.Combine(AttributeReader.GetProjectDirectory(), "SnippetFileFinder/VerifyLambdasAreCalled");
+        var directory = Path.Combine(ProjectFiles.ProjectDirectory, "SnippetFileFinder/VerifyLambdasAreCalled");
         var finder = new FileFinder(
             directory,
             DocumentConvention.SourceTransform,
